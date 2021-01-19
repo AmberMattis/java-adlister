2ce5e2648ce65e702591da1ae19018f84d617780
Amber Mattis <amber.mattis2@gmail.com>
2021-01-19 14:17:39 -0600

    completed page count bonus

--- src/main/java/PageCounterServlet.java
+++ src/main/java/PageCounterServlet.java
@@ -4,11 +4,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "PageCounterServlet", urlPatterns = "/count")
 public class PageCounterServlet extends HttpServlet {
+
+    private int pageCount;
+
+    private void resetCount(){
+        pageCount = 0;
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+
+
+        String reset = req.getParameter("reset");
+
+        if(reset != null){
+            resetCount();
+        }
+        pageCount++;
+
+        out.println("<h1> This page has " + pageCount + " hits!</h1>");
+        }
+
     }
-}
