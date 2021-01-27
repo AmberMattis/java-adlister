e75e169b929f262af6180cf11e308f672d0e9825
Amber Mattis <amber.mattis2@gmail.com>
2021-01-26 18:58:58 -0600

    commented out code

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,14 +1,19 @@
+import java.sql.Connection;
 import java.util.List;
 
-public class MySQLAdsDao implements Ads {
-
-    @Override
-    public List<Ad> all() {
-        return null;
-    }
-
-    @Override
-    public Long insert(Ad ad) {
-        return null;
-    }
-}
+//public class MySQLAdsDao implements Ads {
+//
+//    public MySQLAdsDao(){
+////        Connection connection = new Connection();
+////    }
+////
+////    @Override
+////    public List<Ad> all() {
+////        return null;
+////    }
+////
+////    @Override
+////    public Long insert(Ad ad) {
+////        return null;
+////    }
+//}
